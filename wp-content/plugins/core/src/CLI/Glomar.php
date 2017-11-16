ccba572209de297382cf722fc91460956dd03d9b
Gary Kovar <gkovar@gmail.com>
2017-11-16 09:10:42 -0500

    actually check the correct flags

--- wp-content/plugins/core/src/CLI/Glomar.php
+++ wp-content/plugins/core/src/CLI/Glomar.php
@@ -34,12 +34,12 @@ class Glomar extends Command {
 	public function run_command( $args, $assoc_args ) {
 		$this->config_file = $this->file_system->get_config_file();
 
-		if ( $assoc_args['start'] ) {
-			$this->update_config_file( true );
+		if ( $assoc_args['enable'] ) {
+			$this->update_config_file( 'true' );
 		}
 
-		if ( $assoc_args['stop'] ) {
-			$this->update_config_file( false );
+		if ( $assoc_args['disable'] ) {
+			$this->update_config_file( 'false' );
 		}
 
 		\WP_CLI::line( 'I can neither confirm nor deny that this command has been run' );
