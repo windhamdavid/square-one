9b6067622a71cedb114f5799fe97f4b4d6d03d45
Gary Kovar <gkovar@gmail.com>
2017-11-16 08:58:57 -0500

    adds glomar to service provider

--- wp-content/plugins/core/src/Service_Providers/CLI_Provider.php
+++ wp-content/plugins/core/src/Service_Providers/CLI_Provider.php
@@ -5,6 +5,7 @@ namespace Tribe\Project\Service_Providers;
 use Pimple\Container;
 use Pimple\ServiceProviderInterface;
 use Tribe\Project\CLI\CLI_Generator;
+use Tribe\Project\CLI\Glomar;
 use Tribe\Project\CLI\CPT_Generator;
 use Tribe\Project\CLI\File_System;
 use Tribe\Project\CLI\Pimple_Dump;
@@ -33,6 +34,10 @@ class CLI_Provider implements ServiceProviderInterface {
 			return new CLI_Generator( $container['cli.file-system'] );
 		};
 
+		$container['cli.glomar'] = function ( $container ) {
+			return new Glomar( $container['cli.file-system'] );
+		};
+
 		add_action( 'init', function () use ( $container ) {
 			if ( ! defined( 'WP_CLI' ) || ! WP_CLI ) {
 				return;
@@ -42,6 +47,7 @@ class CLI_Provider implements ServiceProviderInterface {
 			$container['cli.cpt-generator']->register();
 			$container['cli.taxonomy-generator']->register();
 			$container['cli.cli-generator']->register();
+			$container['cli.glomar']->register();
 		}, 0, 0 );
 	}
 }
