acbb5e025ffc522a337a2011f0192472fa201afd
Gary Kovar <gkovar@gmail.com>
2017-11-29 08:21:44 -0500

    adds backend stub

---
+++ wp-content/plugins/core/src/Queues/Backends/SQS.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Tribe\Project\Queues\Backends;
+
+use Tribe\Project\Queues\Contracts\Backend;
+use Tribe\Project\Queues\Message;
+use Aws\Sqs\SqsClient;
+
+class SQS implements Backend {
+
+	protected $sqs = null;
+
+	public function __construct( SqsClient $sqs_client ) {
+		$this->sqs = $sqs_client;
+	}
+
+	private function get_queue_url( $queue_name ) {
+		$queue_url = $this->sqs->createQueue(array(
+			'QueueName' => $queue_name,
+		) );
+
+		return $queue_url->get( 'QueueUrl' );
+	}
+
+	public function get_type(): string {
+		return self::class;
+	}
+
+	public function enqueue( string $queue_name, Message $m ) {
+		$this->get_queue_url( $queue_name );
+
+		$this->sqs->sendMessage();
+	}
+
+	public function dequeue( string $queue_name ) {
+		// TODO: Implement dequeue() method.
+	}
+
+	public function ack( string $job_id, string $queue_name ) {
+		// TODO: Implement ack() method.
+	}
+
+	public function nack( string $job_id, string $queue_name ) {
+		// TODO: Implement nack() method.
+	}
+
+	public function cleanup() {
+		// TODO: Implement cleanup() method.
+	}
+
+	public function count( string $queue_name ): int {
+		// TODO: Implement count() method.
+	}
+}
